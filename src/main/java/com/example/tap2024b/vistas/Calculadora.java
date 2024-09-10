db0ffff9a63f1a497e5f78d72cfe48f89bdada97
Bryan_Cisneros <cisnerosbrayan12345@gmail.com>
2024-09-09 18:03:05 -0600

    esta es la versión donde ya funciona la calculadora parcialmente y tambien abre la loteria aun sin tablero pero el fondo de la aplicacion ya se mira y tambien la app tiene un icono del tecno

--- src/main/java/com/example/tap2024b/vistas/Calculadora.java
+++ src/main/java/com/example/tap2024b/vistas/Calculadora.java
@@ -18,40 +18,90 @@ public class Calculadora extends Stage {
     private String[] strTeclas = {"7", "8", "9", "*", "4", "5", "6", "/", "1", "2", "3", "+", "0", ".", "=", "-"};
     private Button btnClear;
 
-    private void CreateUI(){
+    private void CreateUI() {
         arrBtns = new Button[4][4];
         txtPantalla = new TextField("0");
         txtPantalla.setAlignment(Pos.CENTER_RIGHT);
         gdpTeclado = new GridPane();
         CrearTeclado();
         btnClear = new Button("CE");
-        vBox = new VBox(txtPantalla, gdpTeclado,btnClear);
+        btnClear.setOnAction(event -> txtPantalla.setText("0"));
+        vBox = new VBox(txtPantalla, gdpTeclado, btnClear);
         btnClear.setId("font-button");
-        escena = new Scene(vBox,300,300);
-        escena.getStylesheets().add(getClass().getResource("/styles/cal.css").toString()); //selectores de clase y de id
-                                                //el metodo getResource llama a la carpeta reouscers del proyecto
+        escena = new Scene(vBox, 300, 300);
+        escena.getStylesheets().add(getClass().getResource("/styles/cal.css").toString());
     }
 
-    private void CrearTeclado(){
-        //columna i
+    private void CrearTeclado() {
         for (int i = 0; i < arrBtns.length; i++) {
-            //renglon j
             for (int j = 0; j < arrBtns.length; j++) {
-                arrBtns[j][i] = new Button(strTeclas[4*i+j]);
-                arrBtns[j][i].setPrefSize(75,75);
-                int finalI = i;
-                int finalJ = j;
-                arrBtns[j][i].setOnAction(event -> DetectarTecla(strTeclas[4* finalI + finalJ]));
-                gdpTeclado.add(arrBtns[j][i],j,i);
+                arrBtns[j][i] = new Button(strTeclas[4 * i + j]);
+                arrBtns[j][i].setPrefSize(75, 75);
+                String tecla = strTeclas[4 * i + j];
+                arrBtns[j][i].setOnAction(event -> DetectarTecla(tecla));
+                gdpTeclado.add(arrBtns[j][i], j, i);
             }
         }
     }
 
     private void DetectarTecla(String tecla) {
-        txtPantalla.appendText(tecla);
+        if ("0123456789.".contains(tecla)) {
+            if (txtPantalla.getText().equals("0") || txtPantalla.getText().equals("Error")) {
+                txtPantalla.setText(tecla);
+            } else {
+                txtPantalla.appendText(tecla);
+            }
+        } else if ("+-*/".contains(tecla)) {
+            String text = txtPantalla.getText();
+            if (!text.endsWith(" ") && !text.equals("Error")) {
+                txtPantalla.appendText(" " + tecla + " ");
+            }
+        } else if ("=".equals(tecla)) {
+            try {
+                String resultado = evaluarExpresion(txtPantalla.getText());
+                txtPantalla.setText(resultado);
+            } catch (NumberFormatException e) {
+                txtPantalla.setText("Error");
+            } catch (ArithmeticException e) {
+                txtPantalla.setText("Error");
+            }
+        }
     }
 
-    public Calculadora(){
+    private String evaluarExpresion(String expresion) {
+        String[] tokens = expresion.split(" ");
+        if (tokens.length < 3) return expresion; // Not a valid expression
+
+        double num1 = Double.parseDouble(tokens[0]);
+        double num2 = Double.parseDouble(tokens[2]);
+        String operador = tokens[1];
+
+        double resultado = 0;
+        switch (operador) {
+            case "+":
+                resultado = num1 + num2;
+                break;
+            case "-":
+                resultado = num1 - num2;
+                break;
+            case "*":
+                resultado = num1 * num2;
+                break;
+            case "/":
+                if (num2 != 0) {
+                    resultado = num1 / num2;
+                } else {
+                    throw new ArithmeticException("División por cero");
+                }
+                break;
+            default:
+                throw new IllegalArgumentException("Operador no soportado");
+        }
+
+        return String.valueOf(resultado);
+    }
+
+    public Calculadora() {
         CreateUI();
         this.setTitle("Calculadora");
         this.setScene(escena);
